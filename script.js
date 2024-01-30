07810340ad42ad6856900f34841b53d98dcb5c3f
deondaci <deondaci1@gmail.com>
2024-01-30 08:04:59 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// setInterval(showtate,1000)
+
+function showtate() {
+    let kon = new Date();
+    let hour = kon.getHours();
+    let min = kon.getMinutes();
+    let sec = kon.getSeconds();
+
+    let orma = hour + ":" + min + ":" + sec + sala;
+    l
+
+    let som = document.querySelector('#hov').innerText = orma;
+   
+   
+}
+ setInterval(showtate,1000)
+showtate()
